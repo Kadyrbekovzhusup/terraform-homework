ed4a7869b9903bdacbe5d04ee9c3fba57b4783a0
Kadyrbekovzhusup <kadyrbekovzhusup@gmail.com>
2024-10-22 02:57:36 +0000

    added homework5

---
+++ Homework5/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    region = var.region
+  
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = aws_key_pair.deployer.key_name
+  user_data = file("apache.sh")
+}
